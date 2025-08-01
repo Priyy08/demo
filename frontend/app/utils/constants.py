4ea2ed5233455e0a8a1c6030d00c84792d19d37a
Priyy08 <123939050+Priyy08@users.noreply.github.com>
2025-08-01 23:39:13 +0530

    Update constants.py

--- frontend/app/utils/constants.py
+++ frontend/app/utils/constants.py
@@ -5,11 +5,4 @@ API_BASE_URL = "http://localhost:8000/api" # Use service name 'backend' for Dock
 # IMPORTANT: Replace this with your actual Firebase Web App config
 # You can find this in your Firebase project settings.
 FIREBASE_CONFIG = {
-  "apiKey": "AIzaSyDcT4IfY-WRYQkipo4_mkGVGZo-3KN8zHo",
-  "authDomain": "langchain-realtime-chat.firebaseapp.com",
-  "projectId": "langchain-realtime-chat",
-  "storageBucket": "langchain-realtime-chat.firebasestorage.app",
-  "messagingSenderId": "748821600363",
-  "appId": "1:748821600363:web:e284c1c54617c55fe5f92e",
-  "measurementId": "G-H5XBSZHQRN"
 }
